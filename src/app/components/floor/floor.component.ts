09486a863dc9523ad7e32d1a311916d58652eeaa
ChenZhen <tatuziyun@163.com>
2019-03-01 15:37:39 +0800

    楼栋的页面

--- src/app/components/floor/floor.component.ts
+++ src/app/components/floor/floor.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {RestService} from '../../services/rest.service';
 
 @Component({
   selector: 'app-floor',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./floor.component.scss']
 })
 export class FloorComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns = ['floor', 'title', 'updated_at', 'action'];
+  dataSource: MatTableDataSource<any[]>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(private rest: RestService) {
+    this.loadFloor();
+  }
 
   ngOnInit() {
+
+  }
+  loadFloor() {
+    this.rest.index('floors').subscribe((data: any) => {
+      this.dataSource = new MatTableDataSource(data.result);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  public update (id: string)  {
+    this.rest.navigate(['/bxt/floors/', id, 'edit']);
+  }
+
+  public delete (id: string) {
+    this.rest.confirm({title: 'Are you sure to delete this record?'}).afterClosed().subscribe(res => {
+      if (res) {
+        this.rest.destory('floors/' + id).subscribe(data => {
+          this.loadFloor();
+        }, error => {
+          this.rest.errorHandle(error);
+        });
+      }
+    });
   }
 
 }
