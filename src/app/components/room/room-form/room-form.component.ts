010c32918a8fe7c0f046aa1d606ae97d67efe2bb
“Mengmianxiaoxia <tatuziyun@163.com>
2019-03-04 10:27:23 +0800

    添加了页面.bug暂时未解决

--- src/app/components/room/room-form/room-form.component.ts
+++ src/app/components/room/room-form/room-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {RestService} from '../../../services/rest.service';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-room-form',
@@ -6,10 +10,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-form.component.scss']
 })
 export class RoomFormComponent implements OnInit {
-
-  constructor() { }
-
+  room: any = {id: null, floor: {id: null, house: {id: null}}};
+  floors: Observable<any[]>;
+  houses: Observable<any[]>;
+  constructor(private rest: RestService, private route: ActivatedRoute) { }
   ngOnInit() {
+    this.route.paramMap.subscribe((params: any) => {
+      this.room.id = params.get('id');
+      if (this.room.id != null) {this.edit(); }
+    });
+    this.getHouses();
+    this.getFloors();
   }
+  save() {
+    if (this.room.id != null) {
+      this.update();
+    } else {
+      this.create();
+    }
+  }
+
+  create() {
+    this.rest.create('rooms', this.room).subscribe((data: any) => {
+      this.room = data;
+      this.goBack();
+    }, error => {
+      this.rest.errorHandle(error);
+    });
+  }
+
+  edit() {
+    this.rest.show('rooms/' + this.room.id).subscribe((data: any) => {
+      this.room = data;
+    });
+  }
+
+  update() {
+    this.rest.update('rooms/' + this.room.id, this.room).subscribe((data: any) => {
+      this.room = data;
+      this.goBack();
+    }, error => {
+      this.rest.errorHandle(error);
+    });
+  }
+
+  getHouses() {
+    this.houses = this.rest.index('houses').pipe(map((res: any) =>  res.result ));
+  }
+  getFloors() {
+    if (this.room.floor.house.id) {
+      this.floors = this.rest.index('floors', {house_id: this.room.floor.house.id})
+        .pipe(map((res: any) => res.result));
+    }
+  }
+  selectHouse() {
+    this.getFloors();
+    this.room.parent_id = null;
+  }
+  goBack() {
+    this.rest.navigate(['/bxt/rooms']);
+  }
+
 
 }
--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {RestService} from '../../services/rest.service';
 
 @Component({
   selector: 'app-room',
@@ -6,11 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room.component.scss']
 })
 export class RoomComponent implements OnInit {
-  books: any[];
+  displayedColumns = ['title', 'floor', 'house', 'updated_at', 'action'];
+  dataSource: MatTableDataSource<any[]>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+  constructor(private rest: RestService) {
+  }
 
   ngOnInit() {
+    this.loadRooms();
+  }
+  loadRooms() {
+    this.rest.index('rooms').subscribe((data: any) => {
+      this.dataSource = new MatTableDataSource(data.result);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  public update (id: string)  {
+    this.rest.navigate(['/bxt/rooms/', id, 'edit']);
+  }
+
+  public delete (id: string) {
+    this.rest.confirm({title: 'Are you sure to delete this record?'}).afterClosed().subscribe(res => {
+      if (res) {
+        this.rest.destory('rooms/' + id).subscribe(data => {
+          this.loadRooms();
+        }, error => {
+          this.rest.errorHandle(error);
+        });
+      }
+    });
   }
 
 }
