dd676d0cdfbede72cd78f5e8202b19564d9cb3f9
“Mengmianxiaoxia <tatuziyun@163.com>
2019-03-06 11:53:29 +0800

    添加了学生增加页面

--- src/app/components/student/student-form/student-form.component.ts
+++ src/app/components/student/student-form/student-form.component.ts
@@ -3,16 +3,29 @@ import {map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {RestService} from '../../../services/rest.service';
 import {ActivatedRoute} from '@angular/router';
-
+export interface Gender {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-student-form',
   templateUrl: './student-form.component.html',
   styleUrls: ['./student-form.component.scss']
 })
 export class StudentFormComponent implements OnInit {
-  student: any = {id: null, department: {id: null, college: {id: null}}, tel: null, id_card: null, ic_card: null};
+  student: any = {id: null, department: {id: null, college: {id: null}}, tel: null, id_card: null, ic_card: null, gender: null};
+  genders: Gender[] = [
+    {value: '男', viewValue: '男'},
+    {value: '女', viewValue: '女'}
+  ];
   colleges: Observable<any[]>;
   departments: Observable<any[]>;
+  classrooms: Observable<any[]>;
+
+
+
+
+
   constructor(private rest: RestService, private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -22,7 +35,42 @@ export class StudentFormComponent implements OnInit {
     });
     this.getColleges();
     this.getDepartments();
+    this.getClassrooms();
+
+
+
+
   }
+  getColleges() {
+    this.colleges = this.rest.index('colleges').pipe(map((res: any) =>  res.result ));
+  }
+  getDepartments() {
+    if (this.student.department.college.id) {
+      this.departments = this.rest.index('departments', {college_id: this.student.department.college.id})
+        .pipe(map((res: any) => res.result));
+    }
+  }
+  getClassrooms() {
+    if (this.student.department.college.id) {
+      this.classrooms = this.rest.index('classrooms', {college_id: this.student.department.college.id})
+        .pipe(map((res: any) => res.result));
+    }
+  }
+  selectCollege() {
+    this.getDepartments();
+    this.student.department.id = null;
+  }
+  selectDepartment() {
+    this.getClassrooms();
+    this.student.parent_id = null;
+  }
+
+
+
+
+
+
+
   save() {
     if (this.student.id != null) {
       this.update();
@@ -43,7 +91,6 @@ export class StudentFormComponent implements OnInit {
   edit() {
     this.rest.show('students/' + this.student.id).subscribe((data: any) => {
       this.student = data;
-      this.getDepartments();
     });
   }
 
@@ -56,20 +103,6 @@ export class StudentFormComponent implements OnInit {
     });
   }
 
-  getColleges() {
-    this.colleges = this.rest.index('colleges').pipe(map((res: any) =>  res.result ));
-  }
-  getDepartments() {
-    if (this.student.department.college.id) {
-      this.departments = this.rest.index('departments', {college_id: this.student.department.college.id})
-        .pipe(map((res: any) => res.result));
-    }
-  }
-
-  selectCollege() {
-    this.getDepartments();
-    this.student.parent_id = null;
-  }
 
   goBack() {
     this.rest.navigate(['/bxt/students']);
