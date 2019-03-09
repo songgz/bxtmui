95912b0ecb383043c543a9f98a34b34bc89a66e7
songgz <sgzhe@163.com>
2019-03-09 21:15:46 +0800

    added menu-item

--- src/app/components/menu-item/menu-item-form/menu-item-form.component.ts
+++ src/app/components/menu-item/menu-item-form/menu-item-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RestService} from '../../../services/rest.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-menu-item-form',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-item-form.component.scss']
 })
 export class MenuItemFormComponent implements OnInit {
+  menuItem: any = {id: null};
 
-  constructor() { }
+  constructor(private rest: RestService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: any) => {
+      this.menuItem.id = params.get('id');
+      if (this.menuItem.id != null) {this.edit(); }
+    });
+  }
+
+  create() {
+    this.rest.create('menu_items', this.menuItem).subscribe((data: any) => {
+      this.menuItem = data;
+      this.goBack();
+    }, error => {
+      this.rest.errorHandle(error);
+    });
+  }
+
+  edit() {
+    this.rest.show('menu_items/' + this.menuItem.id).subscribe((data: any) => {
+      this.menuItem = data;
+    });
+  }
+
+  update() {
+    this.rest.update('menu_items/' + this.menuItem.id, this.menuItem).subscribe((data: any) => {
+      this.menuItem = data;
+      this.goBack();
+    }, error => {
+      this.rest.errorHandle(error);
+    });
+  }
+
+  goBack() {
+    this.rest.navigate(['/bxt/menu_items']);
   }
 
 }
