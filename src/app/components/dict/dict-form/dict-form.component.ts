9a5b09ead19b3f8cc63e44401b403264fe8a923f
songgz <sgzhe@163.com>
2019-03-12 06:19:35 +0800

    added role group dict

---
+++ src/app/components/dict/dict-form/dict-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dict-form',
+  templateUrl: './dict-form.component.html',
+  styleUrls: ['./dict-form.component.scss']
+})
+export class DictFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/dicts/dicts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dicts',
+  templateUrl: './dicts.component.html',
+  styleUrls: ['./dicts.component.scss']
+})
+export class DictsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
