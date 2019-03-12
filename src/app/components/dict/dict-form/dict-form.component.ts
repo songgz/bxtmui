56db6f9a140bb13e96d7f5022756f03954f3c6fb
“Mengmianxiaoxia <tatuziyun@163.com>
2019-03-12 11:13:09 +0800

    添加了词典页面

--- src/app/components/dict/dict-form/dict-form.component.ts
+++ src/app/components/dict/dict-form/dict-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {RestService} from '../../../services/rest.service';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-dict-form',
@@ -6,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dict-form.component.scss']
 })
 export class DictFormComponent implements OnInit {
-
-  constructor() { }
+  dict: any = {id: null};
+  constructor(private rest: RestService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: any) => {
+      this.dict.id = params.get('id');
+      if (this.dict.id != null) {this.edit(); }
+    });
+
   }
 
+  save() {
+    if (this.dict.id != null) {
+      this.update();
+    } else {
+      this.create();
+    }
+  }
+
+  create() {
+    this.rest.create('dicts', this.dict).subscribe((data: any) => {
+      this.dict = data;
+      this.goBack();
+    }, error => {
+      this.rest.errorHandle(error);
+    });
+  }
+
+  edit() {
+    this.rest.show('dicts/' + this.dict.id).subscribe((data: any) => {
+      this.dict = data;
+    });
+
+  }
+
+  update() {
+    this.rest.update('dicts/' + this.dict.id, this.dict).subscribe((data: any) => {
+      this.dict = data;
+      this.goBack();
+    }, error => {
+      this.rest.errorHandle(error);
+    });
+  }
+
+
+  goBack() {
+    this.rest.navigate(['/bxt/dicts']);
+  }
 }
