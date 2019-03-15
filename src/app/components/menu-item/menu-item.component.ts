48e5e42a888db23472e978d6e3b912d8be9b43c7
Mengmianxiaoxia <tatuziyun@163.com>
2019-03-15 16:46:35 +0800

    test

--- src/app/components/menu-item/menu-item.component.ts
+++ src/app/components/menu-item/menu-item.component.ts
@@ -8,7 +8,7 @@ import {RestService} from '../../services/rest.service';
   styleUrls: ['./menu-item.component.scss']
 })
 export class MenuItemComponent implements OnInit {
-  displayedColumns = ['title', 'path', 'updated_at', 'action'];
+  displayedColumns = ['title', 'path', 'updated_at', 'action', 'showhidden'];
   dataSource: MatTableDataSource<any[]>;
 
   constructor(private rest: RestService) {
