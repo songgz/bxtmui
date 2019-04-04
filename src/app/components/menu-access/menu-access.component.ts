b14f9e361ac9db74003088d15c9f983f32644a5e
sgzhe <6485785@qq.com>
2019-04-04 14:38:37 +0800

    modify menu access

--- src/app/components/menu-access/menu-access.component.ts
+++ src/app/components/menu-access/menu-access.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material';
 import {RestService} from '../../services/rest.service';
+import {DictService} from '../../services/dict.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-menu-access',
@@ -12,12 +14,16 @@ export class MenuAccessComponent implements OnInit {
   dataSource: MatTableDataSource<any[]>;
   roles: any[] = [];
   permissions: any[] = [];
+  role: any = {id: null};
+  menu_privileges: Observable<any[]>;
+  tests: any[] = ['aa', 'bb'];
 
-  constructor(private rest: RestService) {
+  constructor(private rest: RestService, private dict: DictService) {
     this.dataSource = new MatTableDataSource([]);
   }
 
   ngOnInit() {
+    this.menu_privileges = this.dict.getItems('menu_privilege');
     this.loadRoles();
     this.loadPermissions();
     this.loadMenuItems();
@@ -31,14 +37,26 @@ export class MenuAccessComponent implements OnInit {
 
   loadRoles() {
     this.rest.index('roles').subscribe((data: any) => {
-      this.roles = data;
+      this.roles = data.result;
+      if (this.roles.length > 0) {
+        this.role = this.roles[0];
+      }
     });
   }
 
   loadPermissions() {
     this.rest.index('permissions').subscribe((data: any) => {
-      this.permissions = data;
+      this.permissions = data.result;
     });
+  }
+
+  getPermit(aco_id, aro_id, operation) {
+    for (const perm of this.permissions) {
+      if (perm.aco_id === aco_id && perm.aro_id === aro_id) {
+        return perm.operations.inddexOf(operation) > 0;
+      }
+    }
+    return false;
   }
 
 
