3eb85d474e31feb033bfc325daf022f95f970b95
sgzhe <sgzhe@163.com>
2019-06-13 22:48:00 +0800

    'hgc'

--- src/app/services/excel.service.ts
+++ src/app/services/excel.service.ts
@@ -5,27 +5,13 @@ import * as fs from 'file-saver';
 @Injectable({
   providedIn: 'root'
 })
-export class ExcelService {
-  header = ['Year', 'Month', 'Make', 'Model', 'Quantity', 'Pct']
-  data = [
-    [2007, 1, 'Volkswagen ', 'Volkswagen Passat', 1267, 10],
-    [2007, 1, 'Toyota ', 'Toyota Rav4', 819, 6.5],
-    [2007, 1, 'Toyota ', 'Toyota Avensis', 787, 6.2],
-    [2007, 1, 'Volkswagen ', 'Volkswagen Golf', 720, 5.7],
-    [2007, 1, 'Toyota ', 'Toyota Corolla', 691, 5.4]
-  ];
+export class ExcelService {  
   constructor() { }
 
-  to_excel() {
+  to_excel(header, data) {
     const workbook = new Workbook();
     const worksheet = workbook.addWorksheet('Car Data');
-    const headerRow = worksheet.addRow(this.header);
-    this.data.forEach(d => {
-        const row = worksheet.addRow(d);
-    });
-    workbook.xlsx.writeBuffer().then((data) => {
-      const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-      fs.saveAs(blob, 'CarData.xlsx');
-    });
+    const headerRow = worksheet.addRow(header.keys);
+   
   }
 }
