56c0b80f9e233ec7f70c9738bcdcb4bd505e4e3d
ChenZhen <tatuziyun@163.com>
2019-07-24 09:28:57 +0800

    1.更改样式

--- src/app/components/person-dialog/person-dialog.component.ts
+++ src/app/components/person-dialog/person-dialog.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {RestService} from '../../services/rest.service';
 
 @Component({
   selector: 'app-person-dialog',
   templateUrl: './person-dialog.component.html',
   styleUrls: ['./person-dialog.component.scss']
 })
-export class PersonDialogComponent implements OnInit {
+export class PersonDialogComponent implements OnInit, AfterViewInit {
+  manager: any = { };
+  imgsrc: any = '/assets/img/imghead.png';
 
-  constructor() { }
+  constructor(private rest: RestService) { }
 
   ngOnInit() {
   }
+  inspection() {
+    this.rest.show('managers/' + this.manager.id).subscribe((data: any) => {
+      // 判断图片是否存在
+      const ImgObj = new Image();
+      ImgObj.src = environment.baseUrl + this.manager.avatar_url;
+      if ( data.avatar_url != null && (ImgObj.width > 0 && ImgObj.height > 0) ) {
+        this.imgsrc = environment.baseUrl + this.manager.avatar_url;
+      } else {
+        this.imgsrc = '/assets/img/imghead.png';
+      }
+    });
+  }
 
+  ngAfterViewInit() {
+    this.inspection();
+  }
 }
