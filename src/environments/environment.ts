35b67c68cf714723d959366444d4be2f49a89460
songgz <sgzhe@163.com>
2019-07-27 10:54:45 +0800

    modify env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://127.0.0.1:3000/'
-  // baseUrl: 'http://111.43.134.30:3000/'
+  // baseUrl: 'http://127.0.0.1:3000/'
+  baseUrl: 'http://111.43.134.30:3000/'
 
 };
 
