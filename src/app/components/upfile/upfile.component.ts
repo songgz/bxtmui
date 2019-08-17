4315629c4953f7800fc0bd5d9dbb17806e0bfa45
songgz <sgzhe@163.com>
2019-08-17 15:10:22 +0800

    update angular 8.2

--- src/app/components/upfile/upfile.component.ts
+++ src/app/components/upfile/upfile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UploaderService } from './uploader.service';
 import { MessageService } from './message.service';
+import {Workbook, Worksheet} from 'exceljs';
 
 @Component({
   selector: 'app-upfile',
@@ -16,15 +17,20 @@ export class UpfileComponent implements OnInit {
   ngOnInit() {
   }
   onPicked(input: HTMLInputElement) {
-    const file = input.files[0];
-    if (file) {
-      this.uploaderService.upload(file).subscribe(
-        msg => {
-          input.value = null;
-          this.message = msg;
-        }
-      );
-    }
+    const file: any = input.files[0];
+    const fileReader: FileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+    fileReader.onloadend = function (e: any) {
+      console.log(e.target);
+      const workbook = new Workbook();
+      workbook.xlsx.load(e.target.result)
+        .then(function() {
+          const worksheet = workbook.getWorksheet(0);
+          worksheet.eachRow(function(row, rowNumber) {
+            console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
+          });
+        });
+    };
   }
 
 }
