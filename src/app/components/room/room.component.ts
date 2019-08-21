e820db7ba513f596dac4df46762c202d4f530c8f
ChenZhen <tatuziyun@163.com>
2019-08-21 14:39:24 +0800

    房间管理选择楼层选择

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -15,7 +15,6 @@ import {map} from 'rxjs/operators';
 export class RoomComponent implements OnInit, AfterViewInit {
   displayedColumns = ['title', 'total_beds', 'vacant_beds', 'updated_at', 'action'];
   dataSource: MatTableDataSource<any[]>;
-
   query: any = {};
   houses: Observable<any[]>;
   floors: Observable<any[]>;
@@ -33,7 +32,7 @@ export class RoomComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     // this.loadRooms();
     this.getHouses();
-    this.getFloors();
+    // this.getFloors();
   }
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -60,13 +59,24 @@ export class RoomComponent implements OnInit, AfterViewInit {
   getHouses() {
     this.houses = this.rest.index('houses').pipe(map((res: any) => res.result));
   }
-  getFloors() {
-    this.rest.index('floors').subscribe((data: any) => {
+  getFloors(houseId: string) {
+    const options = {};
+    options['parent_id'] = houseId;
+    this.rest.index('floors', options ).subscribe((data: any) => {
       this.floors = data.result;
     });
   }
+  setFloor() {
+    if (this.query.floor_id) {
+      this.query = JSON.parse(JSON.stringify(this.query).replace(/floor_id/g, 'parent_id'));
+      this.loadRooms(this.query);
+    }
+  }
 
   applyFilter() {
+    if (this.query.house_id) {
+      this.getFloors(this.query.house_id);
+    }
     this.loadRooms(this.query);
   }
 
