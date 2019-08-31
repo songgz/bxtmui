7e9f8d26850e458e040bd2b6002f995ef9b4b13b
songgz <sgzhe@163.com>
2019-09-01 07:04:23 +0800

    modify guard

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -14,5 +14,6 @@ export class AuthGuardService implements CanActivate {
       return true;
     }
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+    return false;
   }
 }
