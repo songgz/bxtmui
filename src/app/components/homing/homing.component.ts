2bb5b86bd6151ed4dffc02b105727802740e76e8
mengmianxiaoxia <tatuziyun@163.com>
2019-09-03 15:16:00 +0800

    1添加楼层房间筛选 2.修复样式

--- src/app/components/homing/homing.component.ts
+++ src/app/components/homing/homing.component.ts
@@ -5,6 +5,8 @@ import {Observable} from 'rxjs';
 import {OrgService} from '../../services/org.service';
 import {DictService} from '../../services/dict.service';
 import {RestService} from '../../services/rest.service';
+import {ImgDialogStudentComponent} from '../student/student.component';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-homing',
@@ -18,6 +20,12 @@ export class HomingComponent implements OnInit, AfterViewInit {
   moreserch = false;
   genders: Observable<any[]>;
   houses: Observable<any[]>;
+  floors:  Observable<any[]>;
+  rooms: Observable<any[]>;
+  house_id = null;
+  floor_id = null;
+  dorm_id = null;
+
   sleep_status: any = {};
   color_status: any = {};
   color_direction: any = {};
@@ -64,8 +72,10 @@ export class HomingComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
+    this.baseUrl = environment.baseUrl;
     this.genders = this.dict.getItems('gender_type');
     this.getHouses();
+    this.getHouseId();
   }
 
   loadHomings(options = {}) {
@@ -83,11 +93,53 @@ export class HomingComponent implements OnInit, AfterViewInit {
   }
 
   applyFilter(filterValue: string = '') {
+    if (this.query.org_id == null) {
+      delete this.query.org_id;
+    }
+    if (this.house_id) {
+      this.query.facility_id = this.house_id;
+      this.dorm_id = null;
+      this.floor_id = null;
+      this.getFloors(this.house_id);
+    }
+    // console.log(this.query);
+    this.loadHomings(this.query);
+  }
+  setFloor() {
+    this.query.facility_id = this.floor_id;
+    this.getRooms(this.floor_id);
+    this.loadHomings(this.query);
+  }
+  setRoom() {
+    this.query.facility_id = this.dorm_id;
     this.loadHomings(this.query);
   }
 
+  getRooms(floorId: string) {
+    const options = {};
+    options['pre'] = 9999;
+    options['parent_id'] = floorId;
+    this.rest.index('rooms', options ).subscribe((data: any) => {
+      // console.log(data.result);
+      this.rooms = data.result;
+    });
+  }
+  getFloors(houseId: string) {
+    const options = {};
+    options['pre'] = 999;
+    options['parent_id'] = houseId;
+    this.rest.index('floors', options ).subscribe((data: any) => {
+      this.floors = data.result;
+    });
+  }
   getHouses() {
     this.houses = this.rest.index('houses').pipe(map((res: any) => res.result));
+  }
+  getHouseId() {
+    this.houses.subscribe( data => {
+      this.house_id = data[0].id;
+      this.applyFilter();
+    })
   }
 
   moreserchbtn() {
@@ -96,6 +148,13 @@ export class HomingComponent implements OnInit, AfterViewInit {
     } else {
       this.moreserch = false;
     }
+  }
+  openDialog(id: string) {
+    this.dialog.open(ImgDialogStudentComponent, {
+      data: {
+        dataid: id
+      }
+    });
   }
 
 }
