3dccaf5e0cb054788e0a3c68b37ae06eb5a6d4fb
mengmianxiaoxia <tatuziyun@163.com>
2020-09-15 10:19:11 +0800

    11111

--- src/app/components/accommodation/student-view/student-view.component.ts
+++ src/app/components/accommodation/student-view/student-view.component.ts
@@ -15,7 +15,7 @@ import {ImgDialogStudentComponent, ListDialogStudentComponent} from '../../stude
   templateUrl: './student-view.component.html',
   styleUrls: ['./student-view.component.scss']
 })
-export class StudentViewComponent implements OnInit , AfterViewInit {
+export class StudentViewComponent implements OnInit {
   disabled = true;
   readOnly = true;
   student: any = { };
@@ -78,7 +78,7 @@ export class StudentViewComponent implements OnInit , AfterViewInit {
     this.rest.show('students/' + this.student.id).subscribe((data: any) => {
       this.student = data;
       this.getRooms();
-      // this.inspection();
+      this.inspection();
     });
   }
   inspection() {
@@ -86,14 +86,11 @@ export class StudentViewComponent implements OnInit , AfterViewInit {
       // 判断图片是否存在
       const ImgObj = new Image();
       ImgObj.src = environment.baseUrl + this.student.avatar_url;
-      if ( ImgObj.src !== null && (ImgObj.width > 0 && ImgObj.height > 0) ) {
+      if ( this.student.avatar_url !== null && (ImgObj.width > 0 && ImgObj.height > 0) ) {
         this.imgsrc = environment.baseUrl + this.student.avatar_url;
       } else {
         this.imgsrc = '/assets/img/imghead.png';
       }
-  }
-  ngAfterViewInit() {
-    this.inspection();
   }
   openDialog() {
     this.dialog.open(ImgDialogStudentComponent, {
